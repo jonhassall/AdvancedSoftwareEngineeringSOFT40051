73f0e01762894d5126fd4f9d32007130db8ccab7
jonhassall <jon@jonhassall.com>
2014-11-26 15:26:49 +0000

    Cleaned up java

--- Java/Practicals/Practical3/src/StringSorting/Sort.java
+++ Java/Practicals/Practical3/src/StringSorting/Sort.java
@@ -2,53 +2,49 @@ package StringSorting;
 
 /**
  *
- * @author Jonathan Hassall
- * TODO:    Tidy up debugging lines and variable names
- *          Look into QuickSort algorithm
- *          Look into any kind of array type with push in Java
- *          Accept user input
+ * @author Jonathan Hassall TODO: Tidy up debugging lines and variable names
+ * Look into QuickSort algorithm Look into any kind of array type with push in
+ * Java Accept user input
  */
-public class Sort {
-    
+public class Sort
+{
     //Note that Java effectively passes by reference on object instances
-    public String [] ascendingSort(String [] s)
+    public String[] ascendingSort(String[] stringArray)
     {
-//        for (int i = 0; i < s.length; i++) {
-        
         int maxLength = 0;
+
         //Find the maximum length
-        for (int i = 0; i < s.length; i++) {
-            System.out.println(s[i] + "Length: " + s[i].length());
-            if (s[i].length() > maxLength)
+        for (int i = 0; i < stringArray.length; i++)
+        {
+            System.out.println(stringArray[i] + "Length: " + stringArray[i].length());
+            if (stringArray[i].length() > maxLength)
             {
-                maxLength = s[i].length();
+                maxLength = stringArray[i].length();
             }
         }
-        
         System.out.println("Maximum length is " + maxLength);
-       
-        String [] sortedByLength = new String[s.length];
+
+        //Make a new array of strings for the sorted list
+        String[] sortedByLength = new String[stringArray.length];
         int sortedByLengthCounter = 0;
-        for (int i = 1; i <= maxLength; i++) {
-            System.out.println("Sorting for length " + i);
-            
-            for (int j = 0; j < s.length; j++) {
-                System.out.println(s[j]);
-                if (s[j].length() == i)
-                {System.out.println("Length match");
-                    sortedByLength[sortedByLengthCounter] = s[j];
+        //Loop around each length from 1 to the maximum length
+        //and insert any strings that match the length into the output array of strings
+        for (int i_targetlength = 1; i_targetlength <= maxLength; i_targetlength++)
+        {
+            System.out.println("Sorting for length " + i_targetlength);
+
+            for (int i = 0; i < stringArray.length; i++)
+            {
+                System.out.println(stringArray[i]);
+                if (stringArray[i].length() == i_targetlength)
+                {
+                    System.out.println("Length match");
+                    sortedByLength[sortedByLengthCounter] = stringArray[i];
                     sortedByLengthCounter++;
                 }
             }
         }
-        
-//        System.out.println("Printing sorted list:");
-//        for (int i = 0; i < sortedByLength.length; i++) {
-//            System.out.println(sortedByLength[i]);
-//        }
-        
+
         return sortedByLength;
-//        s = sortedByLength;
-//        sortedByLength = s;
     }
 }
--- Java/Practicals/Practical3/src/StringSorting/strSort.java
+++ Java/Practicals/Practical3/src/StringSorting/strSort.java
@@ -1,27 +1,29 @@
-/* source file that contains the main function that initializes a string
-   array and calls a sort method (asort()) of another class (sort)
-   to sort the array in ascending order based on string length
-*/
-
+/* Lab exercise: Create a class that sorts this array in order of length
+ */
 package StringSorting;
 
 public class strSort
 {
-   public static void main(String[] args)
-   {
-     // make an array of five strings and intialise it
-      String [] s = {"potato", "cucumber", "onion", "cabbage", "mint"};
-      
-      // create a sort object
-      Sort mySort = new Sort();
-      // call the asort function to sort ascending
-      s = mySort.ascendingSort(s);
-      // print the sorted array
-      
-      System.out.println("Printing sorted list:");
-      
-      for (int i = 0; i < s.length; i++)
-         System.out.println(s[i]);
-   }
-}
 
+    public static void main(String[] args)
+    {
+        // make an array of five strings and intialise it
+        String[] s =
+        {
+            "potato", "cucumber", "onion", "cabbage", "mint"
+        };
+
+        // create a sort object
+        Sort mySort = new Sort();
+        // call the asort function to sort ascending
+        s = mySort.ascendingSort(s);
+        // print the sorted array
+        System.out.println("Printing sorted list:");
+
+        for (int i = 0; i < s.length; i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+
+}
